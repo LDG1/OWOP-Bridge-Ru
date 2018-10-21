6b73c2b3c80c0e950ea9e8f5c1f8aacd6c9c5837
BopItFreak <41522720+BopItFreak@users.noreply.github.com>
2018-10-21 13:47:34 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,106 @@
+var WebSocket = require('ws');
+global.DiscordClient = new (require("discord.js")).Client()
+
+var Client = require('./owopCli.js')
+var owopbot = new Client('ws://whispering-citadel-87343.herokuapp.com');
+//owopbot.world('main')
+owopbot.connect()
+
+setTimeout(function() {
+owopbot.sendMessage('/nick OWOP to Discord')
+owopbot.sendMessage('/adminlogin ifshareibanoof')
+owopbot.move(999999999,999999999)
+},5000)
+var disconnected;
+DiscordClient.login('NDk0MjU1NjAxNzgyNzUxMjU1.DqvW9A.HgqlrxKTA6njg60Tsa4QEYLbPdg');
+DiscordClient.on("disconnect",(event)=>disconnected=true);
+//editing OWOPcli.js wait 
+var lastchat;
+setInterval(function() {
+  if(typeof owopbot.chat == "string") {
+       var channel = DiscordClient.channels.get("502946271045091329");
+  if(lastchat != owopbot.chat && !owopbot.chat.replace(/<(?:.|\n)*?>/gm, '').startsWith('[D]') && !owopbot.chat.replace(/<(?:.|\n)*?>/gm, '').startsWith('Server: You are now an admin. Do /help for a list of commands.') && !owopbot.chat.replace(/<(?:.|\n)*?>/gm, '').startsWith('Nickname') && !owopbot.chat.replace(/<(?:.|\n)*?>/gm, '').startsWith('(AO)') && !owopbot.chat.replace(/<(?:.|\n)*?>/gm, '').startsWith('DEVWorlds'))
+       {
+         
+         console.log("lastchat: "
+          + lastchat + " chat: " +  owopbot.chat)
+         var chat = owopbot.chat.replace(/<(?:.|\n)*?>/gm, '')
+		function say(nessage) {
+		 DiscordClient.channels.get("502946271045091329").send(nessage);      (chat.replace(/(<@[&!]*\d{18}>)|(@everyone)|(@here)/g, '').replace(/\s+/g, ' '))
+		}
+		say(chat.replace(/(<@[&!]*\d{18}>)|(@everyone)|(@here)/g, '').replace(/\s+/g, ' '))
+	   lastchat = owopbot.chat;
+         
+       }
+       //lastchat = owopbot.chat;
+    }
+})
+var auth = {prefix: "!"}
+
+
+
+DiscordClient.on("message", message => {
+    if (message.author.id == "494255601782751255") return;
+    if (message.channel.id != '502946271045091329')
+    return;
+    var msg = message
+   //console.log(message.member.nickname)
+    if (message.member.nickname) {
+      /*owopbot.sendMessage("/sayraw <span style=\"color:#78FFFF\">[D] " + message.member.nickname + ":<span style=\"color: white\"> " + msg)*/
+      owopbot.sendMessage("/sayraw <span style=\"color:#78FFFF\">[D] " + message.member.nickname + ":<span style=\"color: white\"> " + msg)
+    } else {
+    /*owopbot.sendMessage("/sayraw <span style=\"color:#78FFFF\">[D] " + message.author.username + ":<span style=\"color: white\"> " + msg)*/
+    owopbot.sendMessage("/sayraw <span style=\"color:#78FFFF\">[D] " + message.author.username + ":<span style=\"color: white\"> " + msg)
+    }
+})
+DiscordClient.on('message', (msg) => {
+    //if (message.author.id !== bot.user.id) return;
+   msg.a = msg.content;
+	var cmd = msg.a.split(' ')[0].toLowerCase();
+	var input = msg.a.substring(cmd.length).trim();
+	if (cmd == 'o!js') {
+		if (msg.author.id == "246799235775725569") {
+  if (input.startsWith('```js\n')) {
+    input = input.split('```js\n')[1]; 
+  }
+  if (input.endsWith('```')) {
+  input = input.split('```')[0];
+  }
+//{split:{prepend:unescape("%60%60%60js%0A"),append:unescape("%0A%60%60%60")}}
+  //console.log(input)
+    
+	
+		
+		
+		try {
+					let eaa = require("util").inspect(eval(input)).replace(/undefined/g, "unduhfinded");
+				    
+					let h = '```js\n' + '-> ' + eaa  + '\n```'
+					msg.channel.send(h,{split:{prepend:unescape("%60%60%60js%0A"),append:unescape("%0A%60%60%60")}})
+				} catch (e) {
+					msg.channel.send('```diff\n' + '-> ' + e + '\n```');
+				}
+    } else {
+	//msg.react(msg.guild.emojis.get("495399837718806529"));
+	}
+	}
+	})
+function status1() {
+  try{
+  DiscordClient.user.setActivity("augustberchelmann.com/owop", {type: "PLAYING"});
+  } catch(e) {
+    console.log(e)
+  }
+  setTimeout(status2, 10000)
+}
+
+function status2() {
+  try{
+  DiscordClient.user.setActivity("Bop It", {type: "PLAYING"});
+  } catch(e) {
+    console.log(e)
+  }
+  setTimeout(status1, 10000)
+}
+
+setTimeout(status1, 1000)
---
+++ owopCli.js
@@ -0,0 +1,103 @@
+var WebSocket = require('ws');
+
+function Client() {
+  // chunks
+  this.headers = { //headers for WebSocket connection.
+    'Origin': 'http://augustberchelmann.com/owop/'
+  };
+}
+
+Client.prototype = {
+  connect: function() {
+    var opt = {
+      headers: this.headers
+    };
+    if (this.agent) opt.agent = this.agent;
+    if (this.local_address) opt.localAddress = this.local_address;
+
+    this.ws = new WebSocket("ws://whispering-citadel-87343.herokuapp.com", null, opt);
+    this.ws.binaryType = "arraybuffer";
+    this.ws.onopen = this.onConnect.bind(this);
+    this.ws.onmessage = this.onMessage.bind(this);
+    this.ws.onclose = this.onDisconnect.bind(this);
+    this.ws.onerror = this.onError.bind(this);
+  },
+  disconnect: function() {
+    if (!this.ws) {
+      return false;
+    }
+    this.ws.close;
+    return true;
+  },
+  onConnect: function() {
+    var ints = [];
+    if (this.world) {
+      this.world = this.world.toLowerCase();
+    } else {
+      this.world = "main";
+    }
+    for (var i = 0; i < this.world.length && i < 24; i++) {
+      var charCode = this.world.charCodeAt(i);
+      if ((charCode < 123 && charCode > 96) || (charCode < 58 && charCode > 47) || charCode == 95 || charCode == 46) {
+        ints.push(charCode);
+      }
+    }
+    var array = new ArrayBuffer(ints.length + 2);
+    var dv = new DataView(array);
+    for (var i = ints.length; i--;) {
+      dv.setUint8(i, ints[i]);
+    }
+    dv.setUint16(ints.length, 4321, true);
+    this.ws.send(array);
+
+    console.log("Connected! (" + this.index + ") Joining world: " + this.world);
+   //this.index= this
+  },
+  onMessage: function(e) {
+    message = e.data;
+    if (typeof message === "string") { 
+     // console.log(message)
+      this.chat = message
+      return message;
+    }
+    //console.log(message)
+       var dv = new DataView(message);
+    switch (dv.getUint8(0)) {
+      case 0: // Get id
+        this.id = dv.getUint32(1, true);
+        console.log("bot id:", this.id);
+        break;
+    }
+  },
+  onDisconnect: function() {
+    console.log("Disconnected " + this.id + " (" + this.index + ") from server");
+    this.connect()
+  },
+  onError: function(e) {
+    //this.ws.reset();
+    //console.log(e)
+  },
+  move: function(x, y) {
+    var array = new ArrayBuffer(12);
+    var dv = new DataView(array);
+    dv.setInt32(0, x, true);
+    dv.setInt32(4, y, true);
+    dv.setUint8(11, 4);
+    this.ws.send(array);
+  },
+  sendMessage: function(message) {
+    this.ws.send(message + String.fromCharCode(10));
+  },
+  updatePixel: function(x, y, color) {
+    var array = new ArrayBuffer(11);
+    var dv = new DataView(array);
+    dv.setInt32(0, x, true);
+    dv.setInt32(4, y, true);
+    dv.setUint8(8, color[0]);
+    dv.setUint8(9, color[1]);
+    dv.setUint8(10, color[2]);
+    this.ws.send(array);
+  }
+}
+
+module.exports = Client;
